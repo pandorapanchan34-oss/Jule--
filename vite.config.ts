b730e7178985214fa521b7c60d044e74a2e9c56e
pandorapanchan34-oss <pandorapanchan34@gmail.com>
2026-04-11 04:54:49 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   root: "demo",
   base: "/jule-ai-energy/",
   plugins: [react()],
   build: {
     outDir: "../dist",
-    emptyOutDir: true
+    emptyOutDir: true,
   }
 })
