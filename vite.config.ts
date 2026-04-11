80923bf913317a266bb9e9a7cd644bd302643e8b
pandorapanchan34-oss <pandorapanchan34@gmail.com>
2026-04-12 00:50:06 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: 'demo',
-  build: {
+build: {
   outDir: 'dist',  // dist-demo → dist に変更
   emptyOutDir: true,
 },
