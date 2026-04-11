51decbb6c5a58637478cb37cf80f9489824c7ca6
pandorapanchan34-oss <pandorapanchan34@gmail.com>
2026-04-11 23:28:57 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   // ビルドの起点を demo フォルダに設定
-  root: 'demo',
+  root: 'bist-demo',
   plugins: [react()],
   base: '/jule-ai-energy/',
   build: {
